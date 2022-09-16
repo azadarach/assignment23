f0d9b90419b6797b31831c4f784f1adc128e00c2
Alana <110209490+azadarach@users.noreply.github.com>
2022-09-16 16:09:47 +1000

    Add files via upload

--- assignment23/filter.js
+++ assignment23/filter.js
@@ -1,12 +1,12 @@
 function filterCategory(filterSelection, filterBy){
     // filter selection is category
-    // filter by is thing to match 
+    // filter by is thing to match
     let itemsToFilter = Array.from(document.getElementsByClassName("RecipeCardWrapper"));
     for(let listItem of itemsToFilter){
-        if(listItem.dataset.author === filterBy){
-            listItem.style.display = "flex";
+        if(listItem.dataset.author === filterBy){ //filter by author
+            listItem.style.display = "flex"; 
         } else {
-            listItem.style.display = "none";
+            listItem.style.display = "none"; // this says to not display anything if there's no author tag on the filter item
         }
     } // 
 }
@@ -22,7 +22,7 @@ function filterCategory(filterSelection, filterBy){
     // filter by is thing to match 
     let itemsToFilter = Array.from(document.getElementsByClassName("RecipeCardWrapper"));
     for(let listItem of itemsToFilter){
-        if(listItem.dataset.veg === filterBy){
+        if(listItem.dataset.veg === filterBy){ //filter by dietary requirements
             listItem.style.display = "flex";
         } else {
             listItem.style.display = "none";
@@ -41,7 +41,7 @@ function filterAll(){
 //     // filter by is thing to match 
 //     let itemsToFilter = Array.from(document.getElementsByClassName("RecipeCardWrapper"));
 //     for(let listItem of itemsToFilter){
-//         if(listItem.dataset.time === filterBy){
+//         if(listItem.dataset.time === filterBy){ //filter by time
 //             listItem.style.display = "flex";
 //         } else {
 //             listItem.style.display = "none";
@@ -60,7 +60,7 @@ function filterAll(){
 //     // filter by is thing to match 
 //     let itemsToFilter = Array.from(document.getElementsByClassName("RecipeCardWrapper"));
 //     for(let listItem of itemsToFilter){
-//         if(listItem.dataset.cuisine === filterBy){
+//         if(listItem.dataset.cuisine === filterBy){ //filter by cuisine
 //             listItem.style.display = "flex";
 //         } else {
 //             listItem.style.display = "none";
